8f5ccb48155493f938cc2fcb826ac101f8134a32
Christian Kniep <christian@qnib.org>
2017-06-09 11:05:35 +0200

    chmow on tmp-dir

--- opt/qnib/entry/20-create-datanode-dir.sh
+++ opt/qnib/entry/20-create-datanode-dir.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
 echo ">> Bootstrap datanode"
-if [ ! -d /data/hadoopdata/logs ];then
-    mkdir -p /data/hadoopdata/logs/
-    chown -R hadoop: /data/hadoopdata/logs/
-    ln -s /data/hadoopdata/logs /opt/hadoop/
+if [ ! -d /data/hadoop/data/logs ];then
+    mkdir -p /data/hadoop/data/logs/
+    chown -R hadoop: /data/hadoop/data/logs/
+    ln -s /data/hadoop/data/logs /opt/hadoop/
 else
     chown -R hadoop: /opt/hadoop/logs
 fi
 
-if [ ! -d /data/hadoopdata/hdfs/datanode ];then
-    mkdir -p /data/hadoopdata/hdfs/datanode
-    chown -R hadoop: /data/hadoopdata/hdfs/datanode
+if [ ! -d /data/hadoop/data/hdfs/datanode ];then
+    mkdir -p /data/hadoop/data/hdfs/datanode
+    chown -R hadoop: /data/hadoop/data/hdfs/datanode
 else
-    chown -R hadoop: /data/hadoopdata/hdfs/datanode
+    chown -R hadoop: /data/hadoop/data/hdfs/datanode
+fi
+
+if [ ! -d /data/hadoop/tmp/dfs/data ];then
+    mkdir -p /data/hadoop/tmp/dfs/data
+    chown -R hadoop: /data/hadoop/tmp/dfs/data/
 fi
